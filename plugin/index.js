5666400abe194551e5998c4a83c33b4864f7376c
qdl <levinqdl@magicengine.com.cn>
2021-05-28 14:35:05 +0800

    log linked packages

--- plugin/index.js
+++ plugin/index.js
@@ -1,63 +1,70 @@
 /* eslint-env node */
-const path = require("path")
-const list = require('../lib/list')
-const modulesPath = require('../lib/modulesPath')
+const path = require("path");
+const list = require("../lib/list");
+const modulesPath = require("../lib/modulesPath");
 
 class DevResolvePlugin {
   constructor(options) {
-    this.options = options ?? {common: []}
-    this.links = []
-    this.applied = false
+    this.options = options ?? { common: [] };
+    this.links = [];
+    this.applied = false;
   }
   apply(compiler) {
+    const logger = compiler.getInfrastructureLogger("DevResolvePlugin");
     compiler.hooks.watchRun.tapPromise("DevResolvePlugin", async (compiler) => {
-        if (this.applied) return
-      await list(({dest}) => {
-        this.links.push(dest)
-      }).catch(error => {
-        if (error.code !== 'ENOENT') {
-          throw error
+      if (this.applied) return;
+      await list(({ pkgName, dest }) => {
+        this.links.push(dest);
+        logger.info(`${pkgName} -> ${dest}`);
+      }).catch((error) => {
+        if (error.code !== "ENOENT") {
+          throw error;
         }
-      })
+      });
       for (const lib of this.options.common) {
-          const {resolve} = compiler.options
-          resolve.alias = resolve.alias ?? {}
+        const { resolve } = compiler.options;
+        resolve.alias = resolve.alias ?? {};
         resolve.alias[lib] = path.resolve(
           compiler.context,
           "node_modules",
           lib
-        )
+        );
       }
-      compiler.options.resolve.modules.unshift(modulesPath)
-      compiler.options.module.rules.forEach(rule => {
-        rule.exclude = [rule.exclude, filename => {
-          try {
-
-          return this.links.some(link => filename.includes(link))
-          } catch(e) {
-            console.log(e)
-            throw e
-          }
-        }].filter(Boolean)
-      })
+      compiler.options.resolve.modules.unshift(modulesPath);
+      compiler.options.module.rules.forEach((rule) => {
+        rule.exclude = [
+          rule.exclude,
+          (filename) => {
+            try {
+              return this.links.some((link) => filename.includes(link));
+            } catch (e) {
+              console.log(e);
+              throw e;
+            }
+          },
+        ].filter(Boolean);
+      });
       compiler.options.module.rules.unshift({
         test: /\.js$/,
         exclude: [
           /node_modules[\\\/]core-js/,
           /node_modules[\\\/]@babel[\\\/]runtime/,
         ],
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            links: this.links,
-            customize: path.join(__dirname, './custom-loader')
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              links: this.links,
+              customize: path.join(__dirname, "./custom-loader"),
+            },
           },
-        }],
-        include: filename => this.links.some(link => filename.includes(link))
-      })
-      this.applied = true
-    })
+        ],
+        include: (filename) =>
+          this.links.some((link) => filename.includes(link)),
+      });
+      this.applied = true;
+    });
   }
 }
 
-module.exports = DevResolvePlugin
+module.exports = DevResolvePlugin;
